9cff2ea5eadbe9f908bc77b12f55bb919c010882
Gustavo <gustavo.albuquerque@frsp.org>
2021-09-22 20:23:50 -0300

    Atividade Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'atividade01';
-}
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+    })
+    export class AppComponent {
+    nome: any;
+    alterarNome(nome: { target: { value: any; }; }) {
+    console.log(nome.target.value);
+    this.nome = nome.target.value;
+    }
+    adicionar(nomeInput: any) {
+        console.log(nomeInput);
+        console.log("Adicionando...");
+        }
+    }
